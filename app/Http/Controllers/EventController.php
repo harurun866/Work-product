7186f74003f7d52360dbf5c617dffbe8ea4fd24e
harurun866 <harurunkinako0121@gmail.com>
2025-07-13 01:11:56 +0000

    Create an Add Event button

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -11,6 +11,17 @@ class EventController extends Controller
 
     public function show()
     {
-        return view('dashboard');
+        return view('profile.dashboard');
+    }
+    public function create(Request $request)
+    {
+        Event::create([
+            'user_id' => Auth::id(),
+            'body' => $request->input('body'),
+            'date' => $request->input('date'),
+            'is_planned' => $request->input('is_planned'),
+        ]);
+
+        return redirect()->route('show')->with('success', '予定を追加しました。');
     }
 }
--- resources/js/calendar.js
+++ resources/js/calendar.js
@@ -17,16 +17,34 @@ if (calendarEl) {
 
         // カレンダー表示
         initialView: "dayGridMonth", // 最初に表示させるページの形式
+        customButtons: {
+            eventAddButton: {
+                text: "予定を追加",
+                click: function () {
+                    // 初期化（以前入力した値をクリアする）
+                    document.getElementById("body").value = "";
+                    document.getElementById("date").value = "";
+                    document.getElementById("is_planned").value = "0";
+
+                    // モーダルを表示
+                    document.getElementById("modal-add").style.display = "flex";
+                },
+            },
+        },
+
         headerToolbar: {
             // ヘッダーの設定
             // コンマのみで区切るとページ表示時に間が空かず、半角スペースで区切ると間が空く（半角があるかないかで表示が変わることに注意）
             start: "prev,next today", // ヘッダー左（前月、次月、今日の順番で左から配置）
             center: "title", // ヘッダー中央（今表示している月、年）
-            end: "dayGridMonth,timeGridWeek", // ヘッダー右（月形式、時間形式）
+            end: "eventAddButton dayGridMonth,timeGridWeek", // ヘッダー右（月形式、時間形式）
         },
         height: "auto", // 高さをウィンドウサイズに揃える
     });
 
     // カレンダーのレンダリング
     calendar.render();
+    window.closeAddModal = function () {
+        document.getElementById("modal-add").style.display = "none";
+    };
 }

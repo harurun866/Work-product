0a095eba274ea3f4e3a0c87f82f71785a7fd04e1
harurun866 <harurunkinako0121@gmail.com>
2025-07-15 00:16:55 +0000

    Skip duplicate chatrooms migration

--- database/migrations/2025_06_29_024002_create_chatrooms_table.php
+++ database/migrations/2025_06_29_024002_create_chatrooms_table.php
@@ -12,9 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         //Schema::create('chatrooms', function (Blueprint $table) {
-        // $table->id();
-        //  $table->timestamps();
-        // });
+        //$table->id();
+        //$table->timestamps();
+        //});
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //Schema::dropIfExists('chatrooms');
+        // Schema::dropIfExists('chatrooms');
     }
 };
